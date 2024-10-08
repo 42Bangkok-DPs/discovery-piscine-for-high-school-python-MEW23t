8ea951602f7380d5ddcaacf4bae48bd6569c807b
MEW23t <iamkrittiphat@gmail.com>
2024-10-08 11:19:53 +0700

    Update multiplication_table.py

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,1 +1,8 @@
+number = int(input("Enter a number for the multiplication table: "))
 
+print(f"Multiplication table for {number}:")
+
+for i in range(1, 26):
+    result = number * i
+
+    print(f"{number} x {i} = {result}")
