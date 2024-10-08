33e9130671e51e7cbf3e88bfbfb23e43b9a6316f
MEW23t <iamkrittiphat@gmail.com>
2024-10-09 01:52:55 +0700

    Update age.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-a = int(input("Enter your age: "))
+a = int(input("Enter your age"))
 print  ('You will be', a + 10, 'years old in 10 years.') 
 print  ('You will be', a + 20, 'years old in 20 years.') 
 print  ('You will be', a + 30, 'years old in 20 years.')
