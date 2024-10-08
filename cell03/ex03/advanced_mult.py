dc288371ab7b8cf4b447f252474a89625d80465c
MEW23t <iamkrittiphat@gmail.com>
2024-10-08 11:40:43 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,1 +1,6 @@
+for number in range(1, 11):
+    print(f"\nMultiplication table for {number}:")
+    for i in range(1, 11):
+        result = number * i
+        print(f"{number} x {i} = {result}")
 
