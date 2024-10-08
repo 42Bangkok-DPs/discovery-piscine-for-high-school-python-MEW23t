09834c32128610c04faab27a960d32bf1a4bc10f
MEW23t <iamkrittiphat@gmail.com>
2024-10-08 09:28:33 +0700

    Update isneg.py

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,1 +1,9 @@
-
+number = float(input("Please enter a number: "))
+if number > 0:
+    print("plus")
+elif number < 0:
+    print("equal")
+elif number == 00:
+    print("zero")
+else:
+    print("Error")
