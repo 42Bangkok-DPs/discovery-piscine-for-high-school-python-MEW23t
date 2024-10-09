193e857a2e708fcb7184f8c8cd6f5ad106a125ea
MEW23t <iamkrittiphat@gmail.com>
2024-10-09 17:13:29 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python
 
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+plus = set()
+for number in array:
+    if number + 2 > 5:
+        plus.add(number)
+plus_list = list(plus)
+print("array:", array)
+print("Numbers(duplicates):", plus_list)
