4c8eee596fa5ff88ac00cc33ca1a2b4df1237647
MEW23t <iamkrittiphat@gmail.com>
2024-10-10 20:32:45 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+import sys
 
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+        return
+    
+    input_string = sys.argv[1]
+
+    count_z = input_string.count('z')
+    
+    if count_z == 0:
+        print("none")
+    else:
+        print("z" * count_z)
+
+if __name__ == "__main__":
+    main()
