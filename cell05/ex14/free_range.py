7bfa923950dfc70d594ebcbef679f0464ebbe326
MEW23t <iamkrittiphat@gmail.com>
2024-10-10 20:51:21 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+import sys
 
+def main():
+    if len(sys.argv) != 3:
+        print("none")
+        return
+    try:
+        num1 = int(sys.argv[1]) 
+        num2 = int(sys.argv[2])
+    except ValueError:
+        print("none")
+        return
+
+    number = list(range(num1, num2 + 1))
+    print(number)
+
+if __name__ == "__main__":
+    main()
