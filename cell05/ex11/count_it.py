f14669b3bd0588241df529cf0be8246812fa2c81
MEW23t <iamkrittiphat@gmail.com>
2024-10-10 20:54:29 +0700

    Update count_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+import sys
 
+def main():
+    param_count = len(sys.argv) - 1
+    if param_count == 0:
+        print("none")
+    else:
+        print(f"parameters: {param_count}")
+        for param in sys.argv[1:]:
+            print(f"{param} {len(param)}")
+
+if __name__ == "__main__":
+    main()
