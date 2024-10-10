9353eb852f54c215498557f48923d2a4b549dfcb
MEW23t <iamkrittiphat@gmail.com>
2024-10-10 20:51:44 +0700

    Update append_it.py

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+def main():
+    if len(sys.argv) == 1:
+        print("none")
+        return
+    for param in sys.argv[1:]:
+        if not param.endswith("ism"):
+            print(param + "ism")
+if __name__ == "__main__":
+    main()
 
